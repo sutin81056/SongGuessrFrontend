4384f0267811bd62bf0c4847897562f77b22b0c9
Jeff <sutin81056@gmail.com>
2025-03-13 19:46:37 +0800

    v1.0.0

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_BACKEND_URL || "http://localhost:5000");
+const socket = io(
+  process.env.REACT_APP_BACKEND_URL ||
+    "courteous-exploration-production.up.railway.app"
+);
+//const socket = io(process.env.REACT_APP_BACKEND_URL || "http://localhost:5000");
 
 export default socket;
